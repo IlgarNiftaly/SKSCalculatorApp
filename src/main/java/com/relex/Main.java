513137b6f74ca628d2b5508956f33b42830242f3
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2025-03-30 18:43:06 +0400

    re-run method improved. The main() method was being called again, which could cause a StackOverflowError. I restarted the JVM with ProcessBuilder. Pros: The program starts from scratch and cleans up the resources loaded in memory. Cons: It only works properly when run as a .jar file.

--- src/main/java/com/relex/Main.java
+++ src/main/java/com/relex/Main.java
@@ -1,9 +1,10 @@
 package com.relex;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println("Welcome Calculator App");
         System.out.println("existing operation: +, -, *, /, %");
         Scanner scan = new Scanner(System.in);
@@ -13,7 +14,7 @@ public class Main {
         while(true){
             char operation = scan.next().charAt(0);
             if(operation == 'C' || operation == 'c'){
-                main(args);
+                restartApplication();
             }
             double num2 = scan.nextDouble();
 
@@ -24,4 +25,14 @@ public class Main {
         }
 
     }
+
+    public static void restartApplication() throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "java", "-cp", System.getProperty("java.class.path"), "com.relex.Main"
+        );
+        processBuilder.inheritIO(); // Terminal giriş-çıxışını ötürmək
+        processBuilder.start(); // Yeni prosesi başlat
+        System.exit(0); // Mövcud prosesi dayandır
+    }
+
 }
