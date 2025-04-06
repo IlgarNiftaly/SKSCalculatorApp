c55a93b242c2ed4845be5edf9ed50d890f879dbf
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2025-04-06 20:28:05 +0400

    Added sequence operation

--- src/main/java/com/relex/App1.java
+++ src/main/java/com/relex/App1.java
@@ -14,29 +14,35 @@ public class App1 {
             StringBuilder input = new StringBuilder();
             input.append(scan.nextLine().toLowerCase().replaceAll("\\s+", ""));
             StringBuilder operation = new StringBuilder("+");
-            StringBuilder num = new StringBuilder();
+            StringBuilder num = new StringBuilder("0");
 
-            checkOperation(input.toString());
+            checkOperation(input);
             App1 app1 = new App1();
 
             int i = 0;
             while(i < input.length()){
+                operation.append("+");
+                num.append("0");
 
                 if(!Character.isDigit(input.charAt(i))){
                     operation.delete(0, operation.length());
                     while (i < input.length() && !Character.isDigit(input.charAt(i))){
+                        if(operation.length() > 1  && input.charAt(i) == '+'){
+                            break;
+                        }
                         operation.append(input.charAt(i));
                         i++;
                     }
                 }
                 if(Character.isDigit(input.charAt(i))){
+                    num.delete(0, num.length());
                     while (i < input.length() && Character.isDigit(input.charAt(i))){
                         num.append(input.charAt(i));
                         i++;
                     }
                 }
-                checkOperation(input.toString());
-                result = app1.calculate(operation.toString(), Integer.parseInt(num.toString()));
+                checkOperation(operation);
+                result = app1.calculate(operation, Integer.parseInt(num.toString()));
 
                 operation.delete(0, operation.length());
                 num.delete(0, num.length());
@@ -46,27 +52,27 @@ public class App1 {
 
     }
 
-    private static void checkOperation(String operation) throws IOException {
-        if(operation.contains("c")){
+    private static void checkOperation(StringBuilder ope) throws IOException {
+        if(ope.length() > 1){
+            if(ope.charAt(0) == '+' || ope.charAt(0) == '-' || ope.charAt(0) == '*' || ope.charAt(0) == '/' || ope.charAt(0) == '%'){
+                ope.deleteCharAt(0);
+            }
+        }
+        String operation = ope.toString();
+
+        if(operation.equalsIgnoreCase("c")){
             restartApplication();
         }
 
-        if(operation.contains("q")){
+        if(operation.equalsIgnoreCase("q")){
             System.out.println("Proqram sonlandirildi");
             System.exit(0);
         }
     }
 
-    public static void restartApplication() throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder(
-                "java", "-cp", System.getProperty("java.class.path"), "com.relex.App1"
-        );
-        processBuilder.inheritIO(); // Terminal giriş-çıxışını ötürmək
-        processBuilder.start(); // Yeni prosesi başlat
-        System.exit(0); // Mövcud prosesi dayandır
-    }
+    private double calculate(StringBuilder ope, double num){
+        String operation = ope.toString();
 
-    private double calculate(String operation, double num){
         try {
             return switch (operation) {
                 case "+" -> addition(num);
@@ -74,6 +80,7 @@ public class App1 {
                 case "*" -> multiplication(num);
                 case "/" -> division(num);
                 case "%" -> percent(num);
+                case "sq" -> sq(num);
                 default -> {
                     System.out.println("Xeta: Verilenler yanlis daxil edildi!");
                     yield 0;
@@ -105,5 +112,19 @@ public class App1 {
         return result % num;
     }
 
+    private double sq(double num){
+        return result * result;
+    }
+
+
+
+    public static void restartApplication() throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "java", "-cp", System.getProperty("java.class.path"), "com.relex.App1"
+        );
+        processBuilder.inheritIO(); // Terminal giriş-çıxışını ötürmək
+        processBuilder.start(); // Yeni prosesi başlat
+        System.exit(0); // Mövcud prosesi dayandır
+    }
 
 }
